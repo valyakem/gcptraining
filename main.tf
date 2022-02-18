89e098c6d6f4373521176f4f514ce059e2a79f32
valyakem <valyakem@yahoo.co.uk>
2022-02-18 16:57:38 -0500

    changed region

--- main.tf
+++ main.tf
@@ -1,5 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
 provider "aws" {
-  region  = "us-central-1"
+  region  = "ca-central-1"
   access_key = "my-access-key"
   secret_key = "my-secret-key"
 }

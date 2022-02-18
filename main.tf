9387670e4077bf837467c146b9ab8aaaebe3190f
valyakem <valyakem@yahoo.co.uk>
2022-02-18 16:55:27 -0500

    added more info

--- main.tf
+++ main.tf
@@ -1,17 +1,7 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.27"
-    }
-  }
-
-  required_version = ">= 0.14.9"
-}
-
 provider "aws" {
-  profile = "default"
   region  = "us-central-1"
+  access_key = "my-access-key"
+  secret_key = "my-secret-key"
 }
 
 resource "aws_instance" "app_server" {

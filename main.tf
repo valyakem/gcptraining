c3bed3478816e0843a6f04ebdf125c13c15d7a1d
valyakem <valyakem@yahoo.co.uk>
2022-02-18 17:16:50 -0500

    added some files

--- main.tf
+++ main.tf
@@ -1,15 +1,4 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.27"
-    }
-  }
-
-  required_version = ">= 0.14.9"
-}
-
-provider "aws" {
+provider "aws"{
   region  = "ca-central-1"
 }
 

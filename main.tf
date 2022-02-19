627d1f6fd018febed101d8f60ee4bc6c4948a365
valyakem <valyakem@yahoo.co.uk>
2022-02-18 19:54:52 -0500

    committed the files

--- main.tf
+++ main.tf
@@ -11,8 +11,8 @@ provider "google" {
   #credentials = file("<NAME>.json")
 
   project = "gcptrainingproject-340019"
-  region  = "us-central1"
-  zone    = "us-central1-c"
+  region  = "northamerica-northeast2"
+  zone    = "northamerica-northeast2-a"
 }
 
 resource "google_compute_network" "vpc_network" {
